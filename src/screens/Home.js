dc301aee94c687c1c9246c5a2b9605b2fd4e5b82
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2024-04-10 20:31:17 -0300

    .

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,33 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React, { useCallback, useRef } from 'react';
-import SafeContainer from '../components/SafeContainer';
-import MapView from 'react-native-maps';
-import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
+import { Button, Pressable, StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useMemo, useRef } from 'react';
+
+import BottomSheet from '@gorhom/bottom-sheet';
+import { GestureHandlerRootView, PanGestureHandler } from 'react-native-gesture-handler';
 
 export default function Home() {
 
-  const bottomSheetRef = useRef<BottomSheet>(null);
+  const bottomSheetRef = useRef(null);
+  const snapPoints = useMemo(() => ["30%", "80%"], [])
 
-  const handleSheetChanges = useCallback(() => {
-    console.log('handleSheetChanges', index);
-  }, [])
+  const handleCloseAction = () => bottomSheetRef.current?.close();
+  const handleOpenPress = () => bottomSheetRef.current?.expand();
 
   return (
-    <SafeContainer>
-      <View style={estilos.subcontainer}>
-        <MapView style={estilos.map} mapType='terrain' />
-        <BottomSheet
-          ref={bottomSheetRef}
-          onChange={handleSheetChanges}
-        >
-          <BottomSheetView style={estilos.conteudo}>
-            <Text>
-              Awesome!
-            </Text>
-          </BottomSheetView>
-        </BottomSheet>
-      </View>
-    </SafeContainer>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <PanGestureHandler>
+          <View  style={estilos.subcontainer}>
+
+            
+
+            <BottomSheet
+              ref={bottomSheetRef}
+              index={1}
+              snapPoints={snapPoints}
+              backgroundStyle={{ backgroundColor: "#F7F7F7" }}
+            >
+              <View style={estilos.conteudo}>
+                <Text style={{ textAlign: "center", margin: 10 }} >Oi</Text>
+                <Button title='Fechar' onPress={handleCloseAction} />
+              </View>
+            </BottomSheet>
+
+            <Pressable style={estilos.botao} onPress={handleOpenPress}>
+              <Text style={estilos.textoBotao}>Abrir</Text>
+            </Pressable>
+          </View>
+        </PanGestureHandler>
+      </GestureHandlerRootView>
   );
 }
 
@@ -36,9 +45,22 @@ const estilos = StyleSheet.create({
     flex: 1,
     paddingLeft: 0,
     paddingRight: 0,
+    alignItems: "center"
   },
   map: {
     flex: 1,
     width: '100%',    
   },
+  botao: {
+    backgroundColor: "#2b2b2b",
+    width: 120,
+    borderRadius: 10,
+    top: 700
+  },
+  textoBotao: {
+    fontSize: 16,
+    color: "#FFFFFF",
+    paddingVertical: 15,
+    textAlign: "center"
+  }
 });
