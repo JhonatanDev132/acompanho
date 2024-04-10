55c10a64738d0cc72b75f546589e0185ba3e2b40
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2024-04-10 12:00:21 -0300

    .

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,31 @@
-import { StyleSheet, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useRef } from 'react';
 import SafeContainer from '../components/SafeContainer';
 import MapView from 'react-native-maps';
-
+import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
 
 export default function Home() {
+
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  const handleSheetChanges = useCallback(() => {
+    console.log('handleSheetChanges', index);
+  }, [])
+
   return (
     <SafeContainer>
       <View style={estilos.subcontainer}>
         <MapView style={estilos.map} mapType='terrain' />
+        <BottomSheet
+          ref={bottomSheetRef}
+          onChange={handleSheetChanges}
+        >
+          <BottomSheetView style={estilos.conteudo}>
+            <Text>
+              Awesome!
+            </Text>
+          </BottomSheetView>
+        </BottomSheet>
       </View>
     </SafeContainer>
   );
