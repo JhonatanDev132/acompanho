da8d6c0d79fc47e1ae49cfb710637fbda43865f9
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2024-04-10 10:03:23 -0300

    Revert "Update Login.js"
    This reverts commit aee995fcf6e892df9931f85a868ab968128567a3.

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -9,48 +9,41 @@ import {
 import Icon from "../../assets/icon.png";
 import SafeContainer from "../components/SafeContainer";
 import React from "react";
-import LinearGradientComponents from "../components/LinearGradientComponent";
 
 export default function Login({ navigation }) {
   return (
     <SafeContainer>
-      <LinearGradientComponents>
-        <Image source={Icon} style={estilos.logo} />
+      <View style={estilos.logo}>
+        <Image source={Icon} style={estilos.image} />
         <Text style={estilos.texto}>Acompanho</Text>
-          <View>
-            <Text style={estilos.login}>Login</Text>
-          </View>
-          <View style={estilos.formulario}>
-            <Text>E-mail:</Text>
-            <TextInput
-              placeholder="E-mail"
-              style={estilos.input}
-              keyboardType="email-address"
-            />
-            <Text>Senha:</Text>
-            <TextInput placeholder="Senha" style={estilos.input} secureTextEntry />
-          </View>
-        <Pressable style={estilos.botao}>
-          <Text style={estilos.textoBotao}>Entrar</Text>
-        </Pressable>
-        <Pressable onPress={() => navigation.navigate("Inicio")}>
-          <Text>Cadastrar</Text>
-        </Pressable>
-      </LinearGradientComponents>
+      </View>
+      <View style={estilos.formulario}>
+        <Text>E-mail</Text>
+        <TextInput
+          placeholder="E-mail"
+          style={estilos.input}
+          keyboardType="email-address"
+        />
+        <Text>Senha</Text>
+        <TextInput placeholder="Senha" style={estilos.input} secureTextEntry />
+      </View>
+      <Pressable style={estilos.botao}>
+        <Text style={estilos.textoBotao}>Entrar</Text>
+      </Pressable>
+      <Pressable onPress={() => navigation.navigate("Inicio")}>
+        <Text>Cadastrar</Text>
+      </Pressable>
     </SafeContainer>
   );
 }
 
 const estilos = StyleSheet.create({
   logo: {
-    width: 167,
-    height: 130,
+    alignItems: "center",
   },
-  login: {
-    fontSize: 30,
-    fontWeight: "bold",
-    marginBottom: 30,
-    margin: 23,
+  image: {
+    width: 166,
+    height: 130,
   },
   input: {
     backgroundColor: "white",
@@ -61,7 +54,7 @@ const estilos = StyleSheet.create({
     borderRadius: 4,
   },
   formulario: {
-    marginVertical: 16,
+    marginVertical: 22,
     width: "80%",
   },
   botao: {
@@ -78,6 +71,7 @@ const estilos = StyleSheet.create({
     shadowRadius: 3.84,
     elevation: 5,
     margin: 15,
+    marginTop: 25,
   },
   textoBotao: {
     fontSize: 24,
@@ -85,6 +79,7 @@ const estilos = StyleSheet.create({
   },
   texto: {
     fontFamily: "Montserrat",
-    fontSize: 20,
+    fontSize: 30,
+    marginBottom: 35,
   },
 });
