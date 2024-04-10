f3ddd8b31b4a7ecd4f7409db4cc6d80d35b82626
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2024-04-10 09:52:36 -0300

    Update Detalhes.js

--- src/screens/Detalhes.js
+++ src/screens/Detalhes.js
@@ -3,17 +3,20 @@ import React from 'react'
 import SafeContainer from '../components/SafeContainer'
 import logoMini from '../../assets/images/logoMini.png'
 import conectar from '../../assets/images/ilustracao-conectar.png' 
+import LinearGradientComponents from '../components/LinearGradientComponent'
 
 
 export default function Detalhes() {
   return (
     <SafeContainer>
+      <LinearGradientComponents>
         <View style={estilos.subcontainer}>
             <Image source={logoMini}/>
             <Text>Procurar</Text>
             <Text>Utilize a barra de pesquisa para encontrar apoiadores de idosos disponíveis na sua região.</Text>
             <Image source={conectar}/>
         </View>
+      </LinearGradientComponents>
     </SafeContainer>
   )
 }
