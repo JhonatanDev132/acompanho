64755415b8a6e038c60d8fb23fbda46c0258d4ba
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2024-04-10 10:03:20 -0300

    Revert "Update Procurar.js"
    This reverts commit 0251c98ce6b436127d6d1aa293dc78d69177ea9e.

--- src/screens/Procurar.js
+++ src/screens/Procurar.js
@@ -3,13 +3,11 @@ import React from 'react'
 import SafeContainer from '../components/SafeContainer'
 import logoMini from '../../assets/images/logoMini.png'
 import procurar from '../../assets/images/ilustracao-procurar.png' 
-import LinearGradientComponents from '../components/LinearGradientComponent'
 
 
-export default function Procurar({navigation}) {
+export default function Detalhes({navigation}) {
   return (
     <SafeContainer>
-      <LinearGradientComponents>
         <View style={estilos.subcontainer}>
             <Image style={estilos.logo} source={logoMini}/>
             <Text style={estilos.titulo}>Procurar</Text>
@@ -25,7 +23,6 @@ export default function Procurar({navigation}) {
               </Text>
             </Pressable>
         </View>
-      </LinearGradientComponents>
     </SafeContainer>
   )
 }
