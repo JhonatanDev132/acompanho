e58f4ebea9e786485aa3155f912783aa498f152b
Aline Teixeira <131314663+AlineMachadoTeixeira@users.noreply.github.com>
2024-04-15 10:20:26 -0300

    fazendo o css do header da pagina favorito apenas no app.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { SafeAreaView, StatusBar, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
 
 import Inicio from "./src/screens/Inicio";
 import Login from "./src/screens/Login";
@@ -11,73 +11,87 @@ import Seguranca from "./src/screens/Seguranca";
 import Escolha from "./src/screens/Escolha";
 import Home from "./src/screens/Home";
 
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign } from "@expo/vector-icons";
+import Favoritos from "./src/screens/Favoritos";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function StackNavigator() {
   return (
-    <Stack.Navigator initialRouteName="Home">
-    <Stack.Screen
-      name="Login"
-      component={Login}
-      options={{ headerShown: false }}
-    />
-    <Stack.Screen
-      name="Inicio"
-      component={Inicio}
-      options={{ headerShown: false }}
-    />
-    <Stack.Screen
-      name="Escolha"
-      component={Escolha}
-      options={{ headerShown: false }}
-    />
-    <Stack.Screen
-      name="Procurar"
-      component={Procurar}
-      options={{ headerShown: false }}
-    />
-    <Stack.Screen
-      name="Conectar"
-      component={Conectar}
-      options={{ headerShown: false }}
-    />
-    <Stack.Screen
-      name="Seguranca"
-      component={Seguranca}
-      options={{ headerShown: false }}
-    />
-    <Stack.Screen
-      name="HomeScreen"
-      component={TabNavigator}
-      options={{ headerShown: false }}
-    />
-  </Stack.Navigator>
-  )
+    <Stack.Navigator initialRouteName="Favoritos">
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Inicio"
+        component={Inicio}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Escolha"
+        component={Escolha}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Procurar"
+        component={Procurar}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Conectar"
+        component={Conectar}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Seguranca"
+        component={Seguranca}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="HomeScreen"
+        component={TabNavigator}
+        options={{ headerShown: false }}
+      />
+
+      <Stack.Screen
+        name="Favoritos"
+        component={Favoritos}
+        options={{
+          title: "Favoritos",
+          headerStyle: { backgroundColor: "#B8CDE2" },
+          headerTitleAlign: "center",
+          headerTitleStyle: { fontWeight: "bold" },
+        }}
+      />
+    </Stack.Navigator>
+  );
 }
 
 function TabNavigator() {
   return (
-    
     <Tab.Navigator
       screenOptions={{
         tabBarActiveBackgroundColor: "#FAFAFA",
         tabBarInactiveBackgroundColor: "#FAFAFA",
         tabBarActiveTintColor: "#737373",
-        tabBarInactiveTintColor: "#737373"
+        tabBarInactiveTintColor: "#737373",
       }}
     >
-      <Tab.Screen 
-      name="Home" 
-      component={Home} 
-      options={{ headerShown: false, tabBarIcon: () => {
-       return <AntDesign name="home" size={25} color="#737373" />
-      }}}
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown: false,
+          tabBarIcon: () => {
+            return <AntDesign name="home" size={25} color="#737373" />;
+          },
+        }}
       />
     </Tab.Navigator>
-  )
+  );
 }
 
 export default function App() {
@@ -85,7 +99,7 @@ export default function App() {
     <>
       <StatusBar barStyle="light-content" />
       <NavigationContainer>
-        <TabNavigator/>
+        <StackNavigator />
       </NavigationContainer>
     </>
   );
--- src/screens/Favoritos.js
+++ src/screens/Favoritos.js
@@ -1,12 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import SafeContainer from "../components/SafeContainer";
 
 export default function Favoritos() {
   return (
-    <View>
-      <Text>Favoritos</Text>
-    </View>
+    <SafeContainer>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.name}>Favoritos</Text>
+        </View>
+      </View>
+    </SafeContainer>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#fff",
+    width: "100%",
+  },
+
+  header: {
+    alignItems: "center",
+    backgroundColor: "red",
+  },
+
+  name: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+});
