6e82844f51fdce2a38b2096718859444d00b755e
Aline Teixeira <131314663+AlineMachadoTeixeira@users.noreply.github.com>
2024-04-15 09:25:25 -0300

    criando tela favorito

---
+++ src/screens/Favoritos.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function Favoritos() {
+  return (
+    <View>
+      <Text>Favoritos</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
